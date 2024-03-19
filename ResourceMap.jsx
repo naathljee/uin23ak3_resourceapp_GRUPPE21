4fdefd7670466a89435cabdb1ab14a702f4c135e
naathljee <146845810+naathljee@users.noreply.github.com>
2024-03-19 19:20:11 +0100

    La inn koden til å mappe ressuser

---
+++ ResourceMap.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// funksjon for å mappe arrayen med ressurser og skrive det ut til et listeelement 
+function ResourceList({ resources }) {
+
+
+// funksjonen mapper hver ressurs til et listeelement 
+  return (
+    <ul className="resource-list">
+      {resources.map((resource, index) => (
+        <li key={index}>
+          <a href={resource.url} target="_blank" rel="">{resource.title}</a>
+
+        </li>
+      ) ) }
+    </ul>
+  );
+}
+
+export default ResourceList;
