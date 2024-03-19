7c343c22d46f7be180a48375f68b2b3d84502e1e
naathljee <146845810+naathljee@users.noreply.github.com>
2024-03-19 19:14:49 +0100

    Function NavBar

---
+++ NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+
+// funksjon for å liste de ulike valg elementene med NavLink fra react router dom 
+function NavBar() {
+}
+
+export default NavBar;
