2f57b5990ad5fe74f0959d547886e14f375d9ca5
hbakken00 <nikhaug@outlook.com>
2024-03-20 13:17:01 +0100

    Fjernet en import som var unødvendig i RessursArkiv, og endret index.html fane text

--- Gruppe21-test/src/RessursArkiv.jsx
+++ Gruppe21-test/src/RessursArkiv.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './Components/NavBar'
 import Tabinnhold from './Components/Tabinnhold'
